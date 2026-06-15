2aa4e49d3085a173785517c9715f104d3517fb4f
kiriri <3406036+kiriri@users.noreply.github.com>
2026-06-15 22:24:42 +0200

    trying to get jsx types working cross project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Side-effect import: pulls the global `JSX` namespace augmentation into the
+// package's type graph so consumers get JSX.IntrinsicElements automatically.
+import "./core/jsx";
+
 export * from "./core/AB";
 export * from "./core/For";
 export * from "./core/own";
